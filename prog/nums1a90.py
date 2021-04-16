56b33cad7ea8c771a0a1b87ca96c94511eb13c24
Your Name <you@example.com>
2021-04-16 11:33:13 -0300

    funcion que valida nums1a90 y su test

---
+++ prog/nums1a90.py
@@ -0,0 +1,8 @@
+def nums1a90(carton):
+    bandera=1
+    for fila in carton:
+        for celda in fila:
+            if(celda<1 or celda>90):
+                                      bandera=0
+
+    return bandera==1
---
+++ test/test_nums1a90.py
@@ -0,0 +1,6 @@
+from prog.nums1a90 import nums1a90
+
+def test_nums1a90():
+    mi_carton=((1,2,3,4,1,1,1,1,1,),(90,1,1,1,1,1,1,23,34,),(1,54,55,42,90,2,4,3,2,))     
+
+    assert nums1a90(mi_carton)==1
