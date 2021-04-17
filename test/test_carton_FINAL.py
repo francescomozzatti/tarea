3385dd974441ed581c886ff0635a42b69a97dc2d
Your Name <you@example.com>
2021-04-17 15:37:09 -0300

    modifico test para alcanzar el 100% de coveralls

--- test/test_carton_FINAL.py
+++ test/test_carton_FINAL.py
@@ -7,13 +7,27 @@ from prog.carton_FINAL import carton_carton
 from prog.carton_FINAL import nums_orden
 from prog.carton_FINAL import carton_casillas_consecutivas
 
-def test_carton_el_carton():
+def test_carton_el_carton1():
     mi_carton=carton_carton()
-
     assert nums_orden(mi_carton)==1 
+ 
+def test_carton_el_carton2():
+    mi_carton=carton_carton()
     assert carton_casillas_consecutivas(mi_carton)==1 
-    assert nums_15(mi_carton)==1 
+
+def test_carton_el_carton3():
+    mi_carton=carton_carton()
     assert nums_cant_en_columnas(mi_carton)==1 
+
+def test_carton_el_carton4():
+    mi_carton=carton_carton()
     assert nums_cant_en_filas(mi_carton)==1 
+
+def test_carton_el_carton5():
+    mi_carton=carton_carton()
     assert nums_irrepetibles(mi_carton)==1
+
+def test_carton_el_carton6():
+    mi_carton=carton_carton()
     assert nums1a90(mi_carton)==1
+
